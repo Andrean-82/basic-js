986ff2ccc515bcec5c06658c2ce56c2cabc99f5d
Anatoliy Antonov <andrean-82@mail.ru>
2022-05-01 14:59:53 +0500

    task array transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
+  const delbefore = "--discard-prev";
+  const delafter = "--discard-next";
+  const doubleafter = "--double-next";
+  const doublebefore = "--double-prev";
+  let arrNew =[];
+  if (!Array.isArray(arr)) throw new Error ("'arr' parameter must be an instance of the Array!");
+  if(!arr.length) return [];
+  if (arr.length === 0) return arr;
+  
+  for(let elem = 0; elem < arr.length; elem++){
+    
+    if (!arr[elem]) continue;      
+    if (arr[elem] === delbefore){
+      if(arr[elem-1] && arr[elem-2] != delafter){
+        arrNew.pop();
+    }}else if (arr[elem] === delafter){
+      if (arr[elem+1]){
+        elem++;
+    }}else if(arr[elem] === doubleafter){
+      if(arr[elem-1]  && arr[elem+2]){
+        arrNew.push(arr[elem+1]);      
+    }} else if (arr[elem] === doublebefore){
+      if(arr[elem-1] && arr[elem-2] != delafter){
+        arrNew.push(arr[elem-1]);     
+    }}else {arrNew.push(arr[elem]);}
+  } 
+  return arrNew;
 }
-
 module.exports = {
   transform
+  // console.log(transform([1, 2, 3, '--double-next']));
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason(date) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  if (!date){console.log('Unable to determine the time of year!');}
+    try {
+      let month = springDate.getMonth();
+      console.log(month);
+      if(month === undefined){console.log('Unable to determine the time of year!');
+      }
+      if (month == 1 || month == 2 || month == 3){console.log("spring");
+      }
+      if (month == 4 || month == 5 || month == 6){console.log("summer");
+      }
+      if (month == 7 || month == 8 || month == 9){console.log("autumn");
+      }
+      if (month == 10 || month == 11 || month == 0){console.log("winter");
+      }
+    } catch (error) {
+      console.log( "Invalid date!");
+    }
+  }
 
-module.exports = {
+ module.exports = {
+//const springDate = new Date(2222, 555, 25);  //`'spring'`
   getSeason
-};
+ };
+
